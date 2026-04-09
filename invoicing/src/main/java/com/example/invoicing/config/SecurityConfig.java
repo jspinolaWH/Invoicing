7b06258a93e14a2919ebced82a304ac1473f26c2
Jack Spinola <65230037+kokas340@users.noreply.github.com>
2026-04-09 15:44:11 +0200

    cors for vercel sandbox

--- invoicing/src/main/java/com/example/invoicing/config/SecurityConfig.java
+++ invoicing/src/main/java/com/example/invoicing/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://jspinolawh.github.io"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://jspinolawh.github.io","https://wastehero-sandbox.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
